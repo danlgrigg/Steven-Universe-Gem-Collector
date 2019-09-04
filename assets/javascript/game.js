c3ba1175d1136f94547bd0d5abb60b0ce95b5286
danlgrigg <griggdl@gmail.com>
2019-09-04 01:33:58 -0700

    Attached images for background and buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
     //Have random number generate between 19-120 Computer Choice at game start,; reset after each Win/Loss 
     var targetScore = Math.floor(Math.random() * 120) +1;
     var userScore = 0;
-    var winsScore=0;
-    var lossScore=0;
+    var wins = 0;
+    var losses = 0;
 
     //display targetScore and userScore
     $("#target-score-display").text(targetScore);
-    $("#current-score-display").text(userScore);
+    // $("#current-score-display").text(userScore);
         
         
     //Create click events for each Gem button to generate random number 1-12 and store value for each click; reset after game win/loss
@@ -20,33 +20,35 @@ $(document).ready(function(){
     //Each button's value must add to together and display on counter
     $(".button-one").on("click", function(event){
         userScore+= gemChoiceOne;
-        console.log(userScore);
+        $("#current-score-display").text(userScore);
     })
 
     $(".button-two").on("click", function(event){
         userScore+= gemChoiceTwo;
-        console.log(userScore);
+        $("#current-score-display").text(userScore);
     })
 
     $(".button-three").on("click", function(event){
         userScore+= gemChoiceThree;
-        console.log(userScore);
+        $("#current-score-display").text(userScore);
     })
     
     $(".button-four").on("click", function(event){
         userScore+= gemChoiceFour;
-        console.log(userScore);
+        $("#current-score-display").text(userScore);
     })
 
     if (userScore === targetScore) {
-        winScore++;
+        wins++;
+        alert("You chose...Wisely!!")
         userScore=0;
         var targetScore = Math.floor(Math.random() * 120) +1;
                 
     }
 
     if (userScore > targetScore) {
-        lossScore++;
+        alert("You chose...Poorly.");
+        losses++;
         userScore=0;
         var targetScore = Math.floor(Math.random() * 120) +1;
     }
