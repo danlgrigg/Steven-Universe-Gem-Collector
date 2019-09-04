3c7f9f8b963f58a0c57ab2696ecdda11c7199413
danlgrigg <griggdl@gmail.com>
2019-09-04 00:18:48 -0700

    Created JS variables, targetScore, button on-click events; began resets and text to screen.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,55 @@
 $(document).ready(function(){
     //Have random number generate between 19-120 Computer Choice at game start,; reset after each Win/Loss 
-    var computerChoice = Math.random() * (120 - 19) + 19; 
-        console.log(Math.floor(computerChoice));
+    var targetScore = Math.floor(Math.random() * 120) +1;
+    var userScore = 0;
+    var winsScore=0;
+    var lossScore=0;
 
+    //display targetScore and userScore
+    $("#target-score-display").text(targetScore);
+    $("#current-score-display").text(userScore);
+        
+        
     //Create click events for each Gem button to generate random number 1-12 and store value for each click; reset after game win/loss
+    var gemChoiceOne = Math.floor(Math.random() * 12) + 1;
+    var gemChoiceTwo = Math.floor(Math.random() * 12) + 1;
+    var gemChoiceThree= Math.floor(Math.random() * 12) + 1;
+    var gemChoiceFour = Math.floor(Math.random() * 12) + 1;
+
+
     //Each button's value must add to together and display on counter
     $(".button-one").on("click", function(event){
-        console.log(event);
+        userScore+= gemChoiceOne;
+        console.log(userScore);
     })
+
     $(".button-two").on("click", function(event){
-        console.log(event);
+        userScore+= gemChoiceTwo;
+        console.log(userScore);
     })
+
+    $(".button-three").on("click", function(event){
+        userScore+= gemChoiceThree;
+        console.log(userScore);
+    })
+    
+    $(".button-four").on("click", function(event){
+        userScore+= gemChoiceFour;
+        console.log(userScore);
+    })
+
+    if (userScore === targetScore) {
+        winScore++;
+        userScore=0;
+        var targetScore = Math.floor(Math.random() * 120) +1;
+                
+    }
+
+    if (userScore > targetScore) {
+        lossScore++;
+        userScore=0;
+        var targetScore = Math.floor(Math.random() * 120) +1;
+    }
 
     //Create a Reset function; must regenerate Computer Choice and all Gem Choice values, and reset current game score counter
     //Create conditions for Wins Losses and display on Game Counter, apply reset
